4dff5b5ce02c035a31ac2a61a2676274082cf439
natalia000000 <152176051+natalia000000@users.noreply.github.com>
2024-07-11 13:39:42 -0400

    Add files via upload

---
+++ examen.py
@@ -0,0 +1,28 @@
+trabajadores=[]
+print("------Opciones------")
+
+print("1.Asignar sueldos aleatorios")
+print("2.Clasificar sueldos")
+print("3.Ver estadisticas")
+print("4.Reporte de sueldos")
+print("5.Salir del programa")
+
+op=int(input("¿Que opcion desea ingresar? : "))
+
+trabajadores = ["Juan Perez","Maria Garcia","Carlos Lopez","Ana Martinez","Pedro Rodriguez",
+"Laura Hernandez","Miguel Sanchez","Isabel Gomez","Francisco Diaz","Elena Fernandez"]
+
+if op==1:
+        
+    
+    trabajadores = ["Juan Perez",250000,"Maria Garcia",350000,"Carlos Lopez",150000,"Ana Martinez",670000,"Pedro Rodriguez",2300000,
+    "Laura Hernandez",1000000,"Miguel Sanchez",230000,"Isabel Gomez",3000000,"Francisco Diaz",780000,"Elena Fernandez",750000]
+
+
+
+
+    
+
+
+
+
